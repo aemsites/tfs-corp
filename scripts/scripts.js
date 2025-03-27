50f2fb5a2660fe2d829d319647431a31b5564138
Rob Rusher <rrusher@adobe.com>
2025-03-27 08:36:31 -0600

    dup function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,17 +13,8 @@ import {
   toClassName,
 } from './aem.js';
 import {
-  isPreview,
-  inUniversalEditor,
-  moveInstrumentation,
   getTimeoutSignal,
   FETCH_TIMEOUTS,
-  setCookie,
-  getSiteNames,
-  isExternalLink,
-  getPathInfo,
-  fetchLocalePlaceholders,
-  getPlaceholderString,
 } from './utils.js';
 
 const iconLoadingPromises = {};
@@ -94,40 +85,6 @@ export function buildSymbol(name) {
   const icon = document.createElement('i');
   icon.className = `symbol symbol-${toClassName(name)}`;
   return icon;
-}
-
-/**
- * Moves all the attributes from a given elmenet to another given element.
- * @param {Element} from the element to copy attributes from
- * @param {Element} to the element to copy attributes to
- */
-export function moveAttributes(from, to, attributes) {
-  if (!attributes) {
-    // eslint-disable-next-line no-param-reassign
-    attributes = [...from.attributes].map(({ nodeName }) => nodeName);
-  }
-  attributes.forEach((attr) => {
-    const value = from.getAttribute(attr);
-    if (value) {
-      to.setAttribute(attr, value);
-      from.removeAttribute(attr);
-    }
-  });
-}
-
-/**
- * Move instrumentation attributes from a given element to another given element.
- * @param {Element} from the element to copy attributes from
- * @param {Element} to the element to copy attributes to
- */
-export function moveInstrumentation(from, to) {
-  moveAttributes(
-    from,
-    to,
-    [...from.attributes]
-      .map(({ nodeName }) => nodeName)
-      .filter((attr) => attr.startsWith('data-aue-') || attr.startsWith('data-richtext-')),
-  );
 }
 
 /**
